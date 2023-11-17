78ba45baa598f5cdc56bf9ba93bff6148288a4f1
Jack-Briggs-2023 <149538114+Jack-Briggs-2023@users.noreply.github.com>
2023-11-17 04:49:07 +0000

    dad command

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Main {
-    private static void main(String[] args) {
-        System.out.println("HELLO");
+    public static void main(String[] args) {
+        System.out.println("HELLO");System.out.println("HELLO");
     }
 }
